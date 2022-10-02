c19eca1272dfc40aafe5c47c0809cfb09acdef2e
Leonardo Valcarenghi <leonardo.valcarenghi@safeweb.com.br>
2022-10-01 22:09:47 -0300

    buscar cidades mais frias

--- Infraestructure/Queries/Climas/Cidades Mais Frias.sql
+++ Infraestructure/Queries/Climas/Cidades Mais Frias.sql
@@ -1,1 +1,21 @@
 ﻿
+--> OBTER CIDADES MAIS FRIAS HOJE:
+DECLARE @MaisFrias TABLE (Id INT, CidadeId INT, TemperaturaMinima NUMERIC(3, 1));
+INSERT INTO @MaisFrias
+SELECT DISTINCT TOP(@Total) Id, CidadeId, MIN(TemperaturaMinima)
+FROM PrevisaoClima
+WHERE DataPrevisao = CONVERT(DATE, GETDATE())
+GROUP BY Id, CidadeId
+ORDER BY MIN(TemperaturaMinima), CidadeId, Id
+
+--> MONTAR DADOS:
+SELECT 	
+	[MQ].Id,
+	[MQ].TemperaturaMinima,
+	[TemperaturaMaxima] = ( SELECT [TemperaturaMaxima] FROM [PrevisaoClima] WHERE Id = [MQ].Id ),
+	[Clima] = ( SELECT [Clima] FROM [PrevisaoClima] WHERE Id = [MQ].Id ),
+	[DataPrevisao] = ( SELECT [DataPrevisao] FROM [PrevisaoClima] WHERE Id = [MQ].Id ),
+	[UF] = ( SELECT [UF] FROM [Estado] WHERE Id = ( SELECT [EstadoId] FROM [Cidade] WHERE Id = [MQ].CidadeId )),
+	[Cidade] = ( SELECT [Nome] FROM [Cidade] WHERE Id = [MQ].CidadeId )
+	FROM @MaisFrias AS [MQ]
+ORDER BY TemperaturaMinima, [Cidade] ASC
--- Infraestructure/Repository/Climas.cs
+++ Infraestructure/Repository/Climas.cs
@@ -14,7 +14,6 @@ namespace ClimaTempoSimples.Infraestructure.Repository
         private static readonly string _connectionString = "Data Source=localhost; Initial Catalog=ClimaTempoSimples; Integrated Security=True";
 
 
-
         public List<ClimaDTO> ObterMaisQuentes(int total = 3)
         {
             List<ClimaDTO> list = null;
@@ -49,12 +48,39 @@ namespace ClimaTempoSimples.Infraestructure.Repository
             finally { connection.Close(); }
         }
 
-
-
-
         public List<ClimaDTO> ObterMaisFrias(int total = 3)
         {
-            throw new NotImplementedException();
+            List<ClimaDTO> list = null;
+            SqlConnection connection = new SqlConnection(_connectionString);
+            SqlCommand command = new SqlCommand(Queries.Climas.Cidades_Mais_Frias, connection) { CommandType = CommandType.Text };
+            try
+            {
+                connection.Open();
+                command.Parameters.Add(new SqlParameter("@Total", total));
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        if (list is null) { list = new List<ClimaDTO>(); }
+                        list.Add(new ClimaDTO
+                        {
+                            Id = Convert.ToInt32(reader["Id"].ToString()),
+                            TemperaturaMaxima = double.Parse(reader["TemperaturaMaxima"].ToString()),
+                            TemperaturaMinima = double.Parse(reader["TemperaturaMinima"].ToString()),
+                            Clima = reader["Clima"].ToString(),
+                            DataPrevisao = DateTime.Parse(reader["DataPrevisao"].ToString()),
+                            UF = reader["UF"].ToString(),
+                            Cidade = reader["Cidade"].ToString()
+                        });
+                    }
+                    reader.Close();
+                }
+                return list;
+            }
+            catch (SqlException sqlEx) { throw; }
+            catch (Exception ex) { throw; }
+            finally { connection.Close(); }
         }
+
     }
 }
